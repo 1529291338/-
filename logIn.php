c7ff6f94430d3505cf30310136569a125051bb9d
Y <11111@qq.com>
2017-02-18 17:32:01 +0800

    gitone

---
+++ logIn.php
@@ -0,0 +1,42 @@
+
+		<?php
+			header("content-type","text/html;charset=utf-8");
+
+			//1、接受客户端的数据（用户输入的数据）
+			$username = $_REQUEST['idaccount'];
+			$passwordd = $_REQUEST['idpasswordd'];
+			//2、数据保存在数据库中
+			//1）、建立连接（搭桥）
+			$conn = mysql_connect("localhost","root","qianfeng");
+			
+			//2）、选择数据库（找目的地）
+			mysql_select_db("yonghuxinxi",$conn);
+			
+			//3）、传输数据（过桥）
+			//insert语句
+			
+			$sqlstr = "select * from yonghu where username='".$username."'";
+			$result = mysql_query($sqlstr,$conn);
+			$query_num =mysql_num_rows($result);
+			$sqlstr = "select * from yonghu where passwordd='".$passwordd."'";
+			$result = mysql_query($sqlstr,$conn);
+			$query_pass =mysql_num_rows($result);
+
+
+			
+			
+			//4）、关闭连接（拆桥）
+			mysql_close($conn);
+			
+			//三、返回
+			if($query_num==0){
+				echo "0";
+			}else{
+				echo "1";
+			}
+			if($query_pass==0){
+				echo "0";
+			}else{
+				echo "2";
+			}
+		?>
